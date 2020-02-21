1c23485a877c1d02ac959e834bbdd4bed327fe75
kostyashka86 <kostyi_sh@mail.ru>
2020-02-21 21:12:19 +0300

    added browsers

--- addressbook-web-tests/src/test/java/ru/java_course/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/java_course/addressbook/appmanager/ApplicationManager.java
@@ -2,12 +2,16 @@ package ru.java_course.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+    private String browser;
     public WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -15,8 +19,19 @@ public class ApplicationManager {
     private UserHelper userHelper;
     private GroupHelper groupHelper;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         userHelper = new UserHelper(wd);
